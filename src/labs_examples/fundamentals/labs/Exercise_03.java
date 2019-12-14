6fddff1c2a097cb30fb3ca9eb01e8ede9a60f964
Michael Stucki <michaelstucki@Michaels-Mac-mini.local>
2019-12-13 17:58:30 -0700

    completed Exercise_03

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -2,22 +2,25 @@ package labs_examples.fundamentals.labs;
 
 /**
  * Fundamentals Exercise 3: My First Program
- *
- *      Write the necessary code to display the follow message to the console
- *
- *      Hello World!
- *      Check out my first program!
- *
+ * <p>
+ * Write the necessary code to display the follow message to the console
+ * <p>
+ * Hello World!
+ * Check out my first program!
  */
 
 public class Exercise_03 {
 
     public static void main(String[] args) {
 
+        // Write code here
         System.out.println("Hello World!");
+        System.out.println("Check out my first program!");
 
-        // write code to print out "Check out my first program!" below
+        int i = 10;
+        int x = 12;
+        int z = i + x;
 
+        System.out.println(z);
     }
-
 }
