5f6700d0ff4236c058a5b44dff741fff558aa7a2
Michael Stucki <michaelstucki@Michaels-Mac-mini.local>
2019-12-18 16:48:12 -0700

    Started arrays exercises

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,5 +1,7 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
 /**
  * Arrays calculator
  *
@@ -11,5 +13,29 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        final int SIZE = 10;
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int[] numbers = new int[SIZE];
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            numbers[i] = number;
+            sum += number;
+        }
+
+        double average = (double) sum / SIZE;
+
+        System.out.print("Numbers entered: ");
+        for (int number : numbers) {
+            System.out.printf("%d ", number);
+        }
+        System.out.println();
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Average: " + average);
+
+    }
 
 }
